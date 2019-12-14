459c8e345561c8c6d639dc33db82127e77e42b28
Jontahan <jona.jorg@gmail.com>
2019-12-14 15:24:44 +0100

    Separate ris environment from training scripts

--- train_dqn.py
+++ train_dqn.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from vicero.algorithms.reinforce import Reinforce
+from vicero.algorithms.deepqlearning import DQN
 from ris import Koktris
 
 cell_size  = 32
@@ -15,20 +15,21 @@ framerate  = 32
 def plot(history):
         plt.figure(2)
         plt.clf()
-        durations_t = torch.FloatTensor(history)
+        durations_t = torch.DoubleTensor(history)
         plt.title('Training...')
         plt.xlabel('Episode')
         plt.ylabel('Duration')
         plt.plot(durations_t.numpy(), c='lightgray', linewidth=1)
 
-        if len(durations_t) >= 100:
-            means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
-            means = torch.cat((torch.zeros(99), means))
+        his = 100
+        if len(durations_t) >= his:
+            means = durations_t.unfold(0, his, 1).mean(1).view(-1)
+            means = torch.cat((torch.zeros(his - 1), means))
             plt.plot(means.numpy(), c='green')
             
         plt.pause(0.001)
 
-env = Koktris(cell_size, height=22, width=9)
+env = Koktris(cell_size, height=14, width=7)
 
 pg.init()
 clock = pg.time.Clock()
@@ -42,7 +43,7 @@ class PolicyNet(nn.Module):
         self.conv = nn.Conv2d(2, 12, 3)
         self.conv2 = nn.Conv2d(12, 6, 3)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(540, 32)
+        self.fc1 = nn.Linear(180, 32)
         self.fc2 = nn.Linear(32, 16)
         self.fc3 = nn.Linear(16, 5)
 
@@ -53,13 +54,17 @@ class PolicyNet(nn.Module):
         x = torch.flatten(x)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = torch.sigmoid(self.fc3(x))
+        x = self.fc3(x)
+        #x = torch.sigmoid(self.fc3(x))
         return x
 
-poligrad = Reinforce(env, polinet=PolicyNet(), learning_rate=0.01, gamma=0.95, batch_size=5, plotter=plot)
-poligrad.train(10000)
+dqn = DQN(env, qnet=PolicyNet().double(), plotter=plot, render=True, memory_length=20000, )
+dqn.train(1000, 8, plot=True, verbose=True)
 
-while True:
+#poligrad = Reinforce(env, polinet=PolicyNet(), learning_rate=0.01, gamma=0.95, batch_size=5, plotter=plot)
+#poligrad.train(10000)
+
+while False:
     env.draw(screen)
     pg.display.flip()
     action = Koktris.NOP
